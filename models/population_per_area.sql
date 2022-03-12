f7eec7f7dfb90832fd120fb6408f130ec0b91fed
saranglaxman21 <101451375+saranglaxman21@users.noreply.github.com>
2022-03-12 17:20:31 +0530

    Update population_per_area.sql

--- models/population_per_area.sql
+++ models/population_per_area.sql
@@ -1,1 +1,1 @@
-select PROVINCE,Island,AREA_KM_2_ as Area,Population_Density from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source" group by 1,2
+select PROVINCE,Island,AREA_KM_2_ as Area,Population_Density from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source" group by 1,2,3,4
