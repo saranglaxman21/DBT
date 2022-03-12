f357db6761247146b4f5a49f86e7f0870e161aea
saranglaxman21 <101451375+saranglaxman21@users.noreply.github.com>
2022-03-12 17:16:53 +0530

    Update Ratio.sql

--- models/Ratio.sql
+++ models/Ratio.sql
@@ -1,2 +1,2 @@
 {{ config(alias='Ratio_of_Cases',materialized='view', transient=true) }}
-create or replace view Ratio_of_Cases as select PROVINCE,Island,min(Case_Fatality_Rate) as MinimumFatality,max(Case_Recovered_Rate ) as MaxRecovered  from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source" group by 1,2
+select PROVINCE,Island,min(Case_Fatality_Rate) as MinimumFatality,max(Case_Recovered_Rate ) as MaxRecovered  from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source" group by 1,2
