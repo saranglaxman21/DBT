35a750a3c5bbfc18a6bd4c1482759253eb5df9fd
saranglaxman21 <101451375+saranglaxman21@users.noreply.github.com>
2022-03-12 16:00:48 +0530

    Update test.sql

--- models/test.sql
+++ models/test.sql
@@ -1,10 +1,5 @@
---{{ config(alias='Location',materialized='table', transient=true) }}
 
-{{ config(
-    materialized="incremental",
-    schema="marketing"
-) }}
---create table Location as 
+create table Location as 
 select  distinct Continent,Country,Island,PROVINCE
 from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source";
 
