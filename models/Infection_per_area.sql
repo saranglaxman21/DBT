38f6a998ac2b7f960251cdc36f31b7bfd96309fe
saranglaxman21 <101451375+saranglaxman21@users.noreply.github.com>
2022-03-12 17:21:56 +0530

    Update Infection_per_area.sql

--- models/Infection_per_area.sql
+++ models/Infection_per_area.sql
@@ -1,1 +1,1 @@
-select PROVINCE,Island,sum(Total_Active_Cases)/Area as infected_people from {{ ref('Cases') }} join {{'population_per_area'}}
+select a.PROVINCE,a.Island,sum(Total_Active_Cases)/Area as infected_people from {{ ref('Cases') }} a  join {{'population_per_area'}} b group by 1,2
