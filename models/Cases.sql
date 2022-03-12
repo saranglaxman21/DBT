896f9fc25a2a049982d3b8a8d7f0600ad9125849
saranglaxman21 <101451375+saranglaxman21@users.noreply.github.com>
2022-03-12 16:14:40 +0530

    Create Cases.sql

---
+++ models/Cases.sql
@@ -0,0 +1,2 @@
+create or replace view Cases  as 
+select Date,PROVINCE,Island,Sum(Total_Active_Cases) as Total_Active_Cases,Sum(Total_Recovered)  as Total_Recovered from "INTERVIEW_DB"."PLAYGROUND_SARANG_LAXMAN"."main_source" group by 1,2,3
